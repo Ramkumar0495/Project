043bf3c43328a5cbd2a1fadf5ba9eb9a263ec1f1
Ramkumar <ramkumar0495@gmail.com>
2023-03-09 13:03:31 +0530

    district

---
+++ src/org/git/District.java
@@ -0,0 +1,21 @@
+package org.git;
+
+public class District {
+	private void ramnad() {
+System.out.println("ramnad");
+		
+	}
+	private void madurai() {
+		System.out.println("madurai");
+		
+	}
+	private void chennai() {
+		System.out.println("chennai");
+		
+	}
+	private void salem() {
+		System.out.println("salem");
+		
+	}
+
+}
